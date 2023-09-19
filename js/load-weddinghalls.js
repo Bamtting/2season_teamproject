fe5b957db2f0f46457d8b504fcee02161f8b16fe
곽승민 <ekekektmdals@gmail.com>
2023-09-19 14:39:18 +0900

    feat : 서브 웨딩 홀 버튼 기능 적용

--- js/load-weddinghalls.js
+++ js/load-weddinghalls.js
@@ -1,89 +1,188 @@
 $(document).ready(function () {
-  // JSON 파일 1의 경로
-  const jsonFile1 = "json/weddingHalls.json";
+ 
 
-  // JSON 파일 1 데이터를 저장할 변수
-  let weddingHallsData = null;
+  $(".hall-list button").click(function(){
+        
+    $(".hall-list button").removeClass("active");
+    $(this).addClass("active");
+    var selector = $(this).attr("data-filter");
+    $(".mix-wrapper").isotope({
+        filter : selector
+    })
+  })
+    
 
-  // JSON 파일 2의 경로 (서울 지역)
-  const jsonFile2 = "json/seoul.json";
+  // 버튼 클릭 이벤트 처리
+  // $(
+  //   ".hall-btn, .hall-btn-1, .hall-btn-2, .hall-btn-3, .hall-btn-4, .hall-btn-5, .hall2-btn, .hall2-btn-1, .hall2-btn-2, .hall2-btn-3, .hall2-btn-4, .hall2-btn-5"
+  // ).click(function () {
+  //   // 해당 버튼의 data-filter 값을 가져와서 필터링 적용
+  //   let filterValue = $(this).data("filter");
 
-  // 웨딩홀 정보를 출력하는 함수
-  function displayWeddingHalls(selectedLocation, data) {
+  //   // MixItUp을 사용하여 필터링 적용
+  //   mixer.filter(filterValue);
+  // });
+
+  // JSON 데이터 예시 (웨딩홀 정보)
+  const weddingHallData = [
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "호텔",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 500명",
+      id: "all nomal",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel out",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel out",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel out",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel",
+    },
+    {
+      imageSrc: "images/test.jpg",
+      location: "서울 강남구",
+      name: "발라드지디 수서",
+      hallType: "채플",
+      menuType: "뷔페",
+      mealCost: "85,000원",
+      capacity: "최대 400명",
+      id: "chapel",
+    },
+
+    // 다른 웨딩홀 데이터를 추가할 수 있습니다.
+  ];
+
+  // 웨딩홀 정보를 동적으로 생성하고 페이지에 추가하는 함수
+  function createWeddingHallElements(data) {
     const weddingHallsList = $("#weddingHallsList");
-    const weddingHalls = data.weddingHalls;
 
-    weddingHallsList.empty(); // 결과를 비우고 다시 표시
+    data.forEach((hall) => {
+      const hallContainer = $("<div>").addClass(`mix ${hall.id}`);
 
-    weddingHalls.forEach((hall) => {
-      // 선택한 지역과 일치하는 웨딩홀만 표시
-      if (
-        selectedLocation === "전체" ||
-        hall.location.includes(selectedLocation)
-      ) {
-        const hallContainer = $("<div>").addClass(`w-image-box ${hall.catename} `);
+      // 이미지 추가
+      const image = $("<img>")
+        .attr("src", hall.imageSrc)
+        .attr("alt", "웨딩홀 이미지");
+      hallContainer.append(image);
 
-        // 이미지 추가
-        const image = $("<img>").attr("src", hall.image).attr("alt", hall.name);
-        hallContainer.append(image);
+      const leftContainer = $("<div>").addClass("w-box-left");
+      // 위치 정보 추가
+      const location = $("<p>").text(hall.location);
+      leftContainer.append(location);
 
-        const leftContainer = $("<div>").addClass("w-box-left");
-        // 정보 추가
-        const location = $("<p>").text(hall.location);
-        leftContainer.append(location);
+      const name = $("<h3>").text(hall.name);
+      leftContainer.append(name);
 
-        const name = $("<h3>").text(hall.name);
-        leftContainer.append(name);
+      hallContainer.append(leftContainer);
 
-        hallContainer.append(leftContainer);
+      const rightContainer = $("<div>").addClass("w-box-right");
 
-        const rightContainer = $("<div>").addClass("w-box-right");
+      // 홀 종류 추가
+      const hallType = $("<p>").html(`<span>홀 종류: </span>${hall.hallType}`);
+      rightContainer.append(hallType);
 
-        const type = $("<p>").html(`<span>홀 종류: </span>${hall.type}`);
-        rightContainer.append(type);
+      // 메뉴 종류 추가
+      const menuType = $("<p>").html(
+        `<span>메뉴 종류: </span>${hall.menuType}`
+      );
+      rightContainer.append(menuType);
 
-        const menu = $("<p>").html(`<span>메뉴 종류: </span>${hall.menu}`);
-        rightContainer.append(menu);
+      // 식사 비용 추가
+      const mealCost = $("<p>").html(
+        `<span>식사 비용: </span>${hall.mealCost}`
+      );
+      rightContainer.append(mealCost);
 
-        const price = $("<p>").html(`<span>식사 비용: </span>${hall.price}`);
-        rightContainer.append(price);
+      // 보증 인원 추가
+      const capacity = $("<p>").html(
+        `<span>보증 인원: </span>${hall.capacity}`
+      );
+      rightContainer.append(capacity);
 
-        const capacity = $("<p>").html(
-          `<span>보증 인원: </span>${hall.capacity}`
-        );
-        rightContainer.append(capacity);
+      hallContainer.append(rightContainer);
 
-        hallContainer.append(rightContainer);
-
-        weddingHallsList.append(hallContainer);
-      }
+      // 페이지에 추가
+      weddingHallsList.append(hallContainer);
     });
   }
 
-  // JSON 파일 1 불러오기
-  $.getJSON(jsonFile1, function (data1) {
-    // data1은 JSON 파일 1의 데이터입니다.
-    console.log("JSON 파일 1 데이터:", data1);
-    weddingHallsData = data1; // JSON 파일 1 데이터를 변수에 저장
-
-    // 페이지 로드 시 JSON 파일 1 데이터로 웨딩홀 정보 출력
-    displayWeddingHalls("전체", weddingHallsData);
-
-    // "검색" 버튼 클릭 시 JSON 파일 2 (서울) 데이터를 불러와서 처리
-    $("#searchButton").click(function () {
-      $.getJSON(jsonFile2, function (data2) {
-        // data2는 JSON 파일 2 (서울)의 데이터입니다.
-        console.log("JSON 파일 2 데이터 (서울):", data2);
-
-        // 여기에서 data2를 이용해 웨딩홀 정보를 업데이트 또는 필터링할 수 있습니다.
-        // 예를 들어, 선택한 지역이 서울일 때만 해당 정보를 표시하도록 할 수 있습니다.
-        displayWeddingHalls("서울", { weddingHalls: data2.seoulHalls });
-      });
-    });
-
-    // "전체" 버튼 클릭 시 JSON 파일 1 데이터로 돌아감
-    $("#showAllButton").click(function () {
-      displayWeddingHalls("전체", weddingHallsData);
-    });
-  });
+  // 페이지 로드 시 웨딩홀 정보 생성
+  createWeddingHallElements(weddingHallData);
 });
--- js/s_weddinghall.js
+++ js/s_weddinghall.js
@@ -1,69 +1,7 @@
 $(document).ready(function () {
-  // HTML 요소 선택
-  const hallButtons = document.querySelectorAll(
-    ".hall-btn ,.hall-btn-1, .hall-btn-2,.hall-btn-3,.hall-btn-4,.hall-btn-5"
-  );
-  // 초기 상태에서 첫 번째 버튼에 스타일 적용
-  hallButtons[0].style.backgroundColor = "#b99664";
-  hallButtons[0].style.border = "1px solid #b99664";
-  hallButtons[0].style.color = "#fff";
-  hallButtons[0].style.fontWeight = "bold";
-  // 각 버튼에 클릭 이벤트 리스너 추가
-  hallButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      // 모든 버튼의 스타일 초기화
-      hallButtons.forEach((btn) => {
-        btn.style.backgroundColor = "";
-        btn.style.border = "";
-        btn.style.color = "";
-        btn.style.fontWeight = "";
-      });
-
-      // 클릭된 버튼에 스타일 적용
-      const clickedButton = event.target;
-      clickedButton.style.backgroundColor = "#b99664";
-      clickedButton.style.border = "1px solid #b99664";
-      clickedButton.style.color = "#fff";
-      clickedButton.style.fontWeight = "bold";
-
-      // 클릭된 링크의 기본 동작 취소
-      event.preventDefault();
-    });
-  });
-  // HTML 요소 선택
-  const hall2Buttons = document.querySelectorAll(
-    ".hall2-btn ,.hall2-btn-1, .hall2-btn-2,.hall2-btn-3,.hall2-btn-4,.hall2-btn-5"
-  );
-  // 초기 상태에서 첫 번째 버튼에 스타일 적용
-  hall2Buttons[0].style.backgroundColor = "#b99664";
-  hall2Buttons[0].style.border = "1px solid #b99664";
-  hall2Buttons[0].style.color = "#fff";
-  hall2Buttons[0].style.fontWeight = "bold";
-  // 각 버튼에 클릭 이벤트 리스너 추가
-  hall2Buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      // 모든 버튼의 스타일 초기화
-      hall2Buttons.forEach((btn) => {
-        btn.style.backgroundColor = "";
-        btn.style.border = "";
-        btn.style.color = "";
-        btn.style.fontWeight = "";
-      });
-
-      // 클릭된 버튼에 스타일 적용
-      const clickedButton2 = event.target;
-      clickedButton2.style.backgroundColor = "#b99664";
-      clickedButton2.style.border = "1px solid #b99664";
-      clickedButton2.style.color = "#fff";
-      clickedButton2.style.fontWeight = "bold";
-
-      // 클릭된 링크의 기본 동작 취소
-      event.preventDefault();
-    });
-  });
   // 가상의 지역 데이터
   const regions = [
-    { id: "all", name: "전체" },
+    { Velue: "all", name: "전체" },
     { id: "seoul", name: "서울" },
     { id: "gyeonggi", name: "경기" },
     { id: "gangwon", name: "강원" },
